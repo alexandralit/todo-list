c8ba76c16edcefe83ad2ce0329f7e4da25f01d7c
Sasha <aleksikunya@mail.ru>
2021-05-31 22:41:22 +0300

    update save to local storage

--- scripts.js
+++ scripts.js
@@ -41,173 +41,146 @@ function ToDoList() {
     btnReset.innerHTML = 'Reset';
     btnReset.classList.add('reset');
 
-    tasks.forEach(item => {
-        let li = document.createElement('li');
+    if (tasks.length !== 0) tasks.forEach(item => createTask(item));
+
+    function createTask(item) {
+        let task = document.getElementById('text').value;
+        task = task.trim();
+
+        const li = document.createElement('li');
         ul.appendChild(li);
 
-        let checkbox = document.createElement('input');
+        const p = document.createElement('p');
+
+        const checkbox = document.createElement('input');
         checkbox.classList.add('checkbox');
         checkbox.setAttribute('type', 'checkbox');
 
-        let pencil = document.createElement('i');
+        const pencil = document.createElement('i');
         pencil.classList.add('fa');
         pencil.classList.add('fa-pencil');
 
-        let trash = document.createElement('i');
+        const trash = document.createElement('i');
         trash.classList.add('fa');
         trash.classList.add('fa-trash-o');
 
-        p = document.createElement('p');
-        p.innerHTML = item; 
-
-        li.append(checkbox, p, pencil, trash);
-    });
-
-    input.addEventListener('keyup', function(event) { 
-        p = document.createElement('p');
-        p.append(event.target.value); 
-    });
-
-    function addTask() {
-        let li = document.createElement('li');
-        ul.appendChild(li);
-
-        let checkbox = document.createElement('input');
-        checkbox.classList.add('checkbox');
-        checkbox.setAttribute('type', 'checkbox');
-
-        let pencil = document.createElement('i');
-        pencil.classList.add('fa');
-        pencil.classList.add('fa-pencil');
-
-        let trash = document.createElement('i');
-        trash.classList.add('fa');
-        trash.classList.add('fa-trash-o');
-
-        let error = document.createElement('p');
+        const error = document.createElement('p');
         error.classList.add('error');
 
-        if (input.value === '') {
+        if (task === '' && typeof item !== 'string') {
             error.innerHTML = 'Task not added.';
             if (document.getElementsByClassName('error').length === 0) container.insertBefore(error, btn);
             li.remove();
+        } else if (typeof item === 'string') {
+            if (document.querySelector('.error')) document.querySelector('.error').remove();
+            p.innerHTML = item;
+            li.append(checkbox, p, pencil, trash);
+            deleteTask(trash);
+            checkedTask(checkbox);
+            editTask(pencil);
         } else {
             if (document.querySelector('.error')) document.querySelector('.error').remove();
+            p.innerHTML = task;
             li.append(checkbox, p, pencil, trash);
-            tasks.push(p.textContent);
+            deleteTask(trash);
+            checkedTask(checkbox);
+            editTask(pencil);
+            tasks.push(task);
             localStorage.setItem('Tasks', JSON.stringify(tasks));
             input.value = '';
         }
+    }
 
-        pencil.addEventListener('click', openModalWindow);
-
-        checkbox.onchange = function() {
-            if (checkbox.checked) checkbox.parentElement.classList.add('checked'); 
-            else checkbox.parentElement.classList.remove('checked'); 
-        };
-
-        trash.addEventListener('click', function() {
-            trash.parentElement.remove();
+    function deleteTask(element) {
+        element.addEventListener('click', (event) => {
+            element.parentElement.remove();
+            event.stopPropagation();
             tasks.map((item, index, array) => {
-                if (trash.parentElement.querySelector('p').textContent === item) return array.splice(index, 1);
+                if (element.parentElement.querySelector('p').textContent === item) return array.splice(index, 1);
             });
             localStorage.setItem('Tasks', JSON.stringify(tasks));
         });
-    };
+    }
+
+    function checkedTask(element) {
+        element.onchange = function() {
+            if (element.checked) element.parentElement.classList.add('checked'); 
+            else element.parentElement.classList.remove('checked'); 
+        };
+    }
+
+    function editTask(element) {
+        element.addEventListener('click', openModalWindow);
+    }
 
     input.addEventListener('keyup', (event) => {
-        if (event.keyCode === 13) addTask();
+        if (event.keyCode === 13) createTask();
     });
 
-    btnAdd.addEventListener('click', addTask);
+    btnAdd.addEventListener('click', createTask);
 
-    const arrLi = document.querySelectorAll('.list li');
     btnReset.addEventListener('click', () => {
-        arrLi.forEach(item => item.remove());
+        ul.innerHTML = '';
         localStorage.removeItem('Tasks');
-    });
-
-    const arrCheckbox = document.querySelectorAll('.list input');
-    arrCheckbox.forEach(item => {
-        item.onchange = function() {
-            if (item.checked) item.parentElement.classList.add('checked'); 
-            else item.parentElement.classList.remove('checked'); 
-        };
-    });
-
-    const arrPencil = document.querySelectorAll('.fa-pencil');
-    arrPencil.forEach(item => {
-        item.addEventListener('click', openModalWindow);
     });
 
     function openModalWindow(event) {
         container.classList.remove('fadeIn');
 
-            let modalWindow = document.createElement('div'),
-                modalWindowWrapper = document.createElement('div'),
-                modalWindowInput = document.createElement('input'),
-                buttonClose = document.createElement('i'),
-                buttonSave = document.createElement('button'),
-                overlay = document.createElement('div');
+        let modalWindow = document.createElement('div'),
+            modalWindowWrapper = document.createElement('div'),
+            modalWindowInput = document.createElement('input'),
+            buttonClose = document.createElement('i'),
+            buttonSave = document.createElement('button'),
+            overlay = document.createElement('div');
 
-            document.body.appendChild(modalWindow);
-            modalWindow.classList.add('modalWindow');
+        document.body.appendChild(modalWindow);
+        modalWindow.classList.add('modalWindow');
 
-            document.body.appendChild(overlay);
-            overlay.classList.add('overlay');
+        document.body.appendChild(overlay);
+        overlay.classList.add('overlay');
 
-            modalWindow.appendChild(modalWindowWrapper);
-            modalWindowWrapper.classList.add('modalWindowWrapper');
+        modalWindow.appendChild(modalWindowWrapper);
+        modalWindowWrapper.classList.add('modalWindowWrapper');
             
-            modalWindowWrapper.appendChild(modalWindowInput);
-            modalWindowInput.setAttribute('type', 'text');
-            modalWindowInput.setAttribute('id', 'textEdit');
-            modalWindowInput.value = event.target.parentElement.querySelector('p').textContent;
+        modalWindowWrapper.appendChild(modalWindowInput);
+        modalWindowInput.setAttribute('type', 'text');
+        modalWindowInput.setAttribute('id', 'textEdit');
+        modalWindowInput.value = event.target.parentElement.querySelector('p').textContent;
 
-            modalWindowWrapper.appendChild(buttonSave);
-            buttonSave.classList.add('save');
-            buttonSave.innerHTML = 'Save the changes';
+        modalWindowWrapper.appendChild(buttonSave);
+        buttonSave.classList.add('save');
+        buttonSave.innerHTML = 'Save the changes';
 
-            function closeModalWindow() {
-                tasks.map((item, index, array) => {
-                    if (event.target.parentElement.querySelector('p').textContent === item) {
-                        return array.splice(index, 1, modalWindowInput.value);
-                    } 
-                });
-                localStorage.setItem('Tasks', JSON.stringify(tasks));
-                event.target.parentElement.querySelector('p').innerHTML = modalWindowInput.value;
-                overlay.remove();
-                modalWindow.remove();
-                container.classList.add('fadeIn');
-            }
-
-            buttonSave.addEventListener('click', closeModalWindow);
-
-            modalWindowInput.addEventListener('keyup', (event) => {
-                if (event.keyCode === 13) closeModalWindow();
-            });
-
-            modalWindowWrapper.appendChild(buttonClose);
-            buttonClose.classList.add('fa');
-            buttonClose.classList.add('fa-times');
-        
-            buttonClose.addEventListener('click', function() {
-                overlay.remove();
-                modalWindow.remove();
-                container.classList.add('fadeIn');
-            });
-    }
-
-    const arrTrash = document.querySelectorAll('.fa-trash-o');
-    arrTrash.forEach(item => {
-        item.addEventListener('click', function(event) {
-            event.target.parentElement.remove();
+        function closeModalWindow() {
             tasks.map((item, index, array) => {
-                if (event.target.parentElement.querySelector('p').textContent === item) return array.splice(index, 1);
+                if (event.target.parentElement.querySelector('p').textContent === item) {
+                    return array.splice(index, 1, modalWindowInput.value);
+                } 
             });
             localStorage.setItem('Tasks', JSON.stringify(tasks));
+            event.target.parentElement.querySelector('p').innerHTML = modalWindowInput.value;
+            overlay.remove();
+            modalWindow.remove();
+            container.classList.add('fadeIn');
+        }
+
+        buttonSave.addEventListener('click', closeModalWindow);
+
+        modalWindowInput.addEventListener('keyup', (event) => {
+            if (event.keyCode === 13) closeModalWindow();
         });
-    });
+
+        modalWindowWrapper.appendChild(buttonClose);
+        buttonClose.classList.add('fa');
+        buttonClose.classList.add('fa-times');
+        
+        buttonClose.addEventListener('click', function() {
+            overlay.remove();
+            modalWindow.remove();
+            container.classList.add('fadeIn');
+        });
+    }
 };
 
 window.addEventListener('load', ToDoList);
