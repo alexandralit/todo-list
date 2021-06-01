089c5645693bc141caa159fa1982b21dd09fca7a
Sasha <aleksikunya@mail.ru>
2021-06-01 21:37:07 +0300

    added a dark theme

--- scripts.js
+++ scripts.js
@@ -1,186 +1,193 @@
-function ToDoList() {
-    let tasks = JSON.parse(localStorage.getItem('Tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem('Tasks')) || [];
 
-    let section = document.createElement('section'),
-        container = document.createElement('div'),
-        title = document.createElement('p'),
-        div = document.createElement('div'),
-        input = document.createElement('input'),
-        ul = document.createElement('ul'),
-        btn = document.createElement('div'),
-        btnAdd = document.createElement('button'),
-        btnReset = document.createElement('button');
+const section = document.createElement('section'),
+    container = document.createElement('div'),
+    title = document.createElement('p'),
+    div = document.createElement('div'),
+    input = document.createElement('input'),
+    ul = document.createElement('ul'),
+    btn = document.createElement('div'),
+    btnAdd = document.createElement('button'),
+    btnReset = document.createElement('button');
 
-    document.body.appendChild(section);
+document.body.appendChild(section);
 
-    section.appendChild(container);
-    container.classList.add('container');
+section.appendChild(container);
+container.classList.add('container');
 
-    title.innerHTML = 'ToDo List';
-    container.appendChild(title);
+title.innerHTML = 'ToDo List';
+container.appendChild(title);
 
-    container.appendChild(div);
-    div.classList.add('block');
-    
-    div.appendChild(input);
-    input.setAttribute('type', 'text');
-    input.setAttribute('id', 'text');
-    input.setAttribute('placeholder', 'Type your task');
+container.appendChild(div);
+div.classList.add('block');
 
-    container.appendChild(ul);
-    ul.classList.add('list');
+div.appendChild(input);
+input.setAttribute('type', 'text');
+input.setAttribute('id', 'text');
+input.setAttribute('placeholder', 'Type your task');
 
-    container.appendChild(btn);
-    btn.classList.add('button');
+container.appendChild(ul);
+ul.classList.add('list');
 
-    btn.appendChild(btnAdd);
-    btnAdd.innerHTML = 'Add';
-    btnAdd.classList.add('add');
+container.appendChild(btn);
+btn.classList.add('button');
 
-    btn.appendChild(btnReset);
-    btnReset.innerHTML = 'Reset';
-    btnReset.classList.add('reset');
+btn.appendChild(btnAdd);
+btnAdd.innerHTML = 'Add';
+btnAdd.classList.add('add');
 
-    if (tasks.length !== 0) tasks.forEach(item => createTask(item));
+btn.appendChild(btnReset);
+btnReset.innerHTML = 'Reset';
+btnReset.classList.add('reset');
 
-    function createTask(item) {
-        let task = document.getElementById('text').value;
-        task = task.trim();
+if (tasks.length !== 0) tasks.forEach(item => createTask(item));
 
-        const li = document.createElement('li');
-        ul.appendChild(li);
+function createTask(item) {
+    let task = document.getElementById('text').value;
+    task = task.trim();
 
-        const p = document.createElement('p');
+    const li = document.createElement('li');
+    ul.appendChild(li);
 
-        const checkbox = document.createElement('input');
-        checkbox.classList.add('checkbox');
-        checkbox.setAttribute('type', 'checkbox');
+    const p = document.createElement('p');
 
-        const pencil = document.createElement('i');
-        pencil.classList.add('fa');
-        pencil.classList.add('fa-pencil');
+    const checkbox = document.createElement('input');
+    checkbox.classList.add('checkbox');
+    checkbox.setAttribute('type', 'checkbox');
 
-        const trash = document.createElement('i');
-        trash.classList.add('fa');
-        trash.classList.add('fa-trash-o');
+    const pencil = document.createElement('i');
+    pencil.classList.add('fa');
+    pencil.classList.add('fa-pencil');
 
-        const error = document.createElement('p');
-        error.classList.add('error');
+    const trash = document.createElement('i');
+    trash.classList.add('fa');
+    trash.classList.add('fa-trash-o');
 
-        if (task === '' && typeof item !== 'string') {
-            error.innerHTML = 'Task not added.';
-            if (document.getElementsByClassName('error').length === 0) container.insertBefore(error, btn);
-            li.remove();
-        } else if (typeof item === 'string') {
-            if (document.querySelector('.error')) document.querySelector('.error').remove();
-            p.innerHTML = item;
-            li.append(checkbox, p, pencil, trash);
-            deleteTask(trash);
-            checkedTask(checkbox);
-            editTask(pencil);
-        } else {
-            if (document.querySelector('.error')) document.querySelector('.error').remove();
-            p.innerHTML = task;
-            li.append(checkbox, p, pencil, trash);
-            deleteTask(trash);
-            checkedTask(checkbox);
-            editTask(pencil);
-            tasks.push(task);
-            localStorage.setItem('Tasks', JSON.stringify(tasks));
-            input.value = '';
-        }
+    const error = document.createElement('p');
+    error.classList.add('error');
+
+    if (task === '' && typeof item !== 'string') {
+        error.innerHTML = 'Task not added.';
+        if (document.getElementsByClassName('error').length === 0) container.insertBefore(error, btn);
+        li.remove();
+    } else if (typeof item === 'string') {
+        if (document.querySelector('.error')) document.querySelector('.error').remove();
+        p.innerHTML = item;
+        li.append(checkbox, p, pencil, trash);
+        deleteTask(trash);
+        checkedTask(checkbox);
+        editTask(pencil);
+    } else {
+        if (document.querySelector('.error')) document.querySelector('.error').remove();
+        p.innerHTML = task;
+        li.append(checkbox, p, pencil, trash);
+        deleteTask(trash);
+        checkedTask(checkbox);
+        editTask(pencil);
+        tasks.push(task);
+        localStorage.setItem('Tasks', JSON.stringify(tasks));
+        input.value = '';
     }
+}
 
-    function deleteTask(element) {
-        element.addEventListener('click', (event) => {
-            element.parentElement.remove();
-            event.stopPropagation();
-            tasks.map((item, index, array) => {
-                if (element.parentElement.querySelector('p').textContent === item) return array.splice(index, 1);
-            });
-            localStorage.setItem('Tasks', JSON.stringify(tasks));
+function deleteTask(element) {
+    element.addEventListener('click', (event) => {
+        element.parentElement.remove();
+        event.stopPropagation();
+        tasks.map((item, index, array) => {
+            if (element.parentElement.querySelector('p').textContent === item) return array.splice(index, 1);
         });
-    }
-
-    function checkedTask(element) {
-        element.onchange = function() {
-            if (element.checked) element.parentElement.classList.add('checked'); 
-            else element.parentElement.classList.remove('checked'); 
-        };
-    }
-
-    function editTask(element) {
-        element.addEventListener('click', openModalWindow);
-    }
-
-    input.addEventListener('keyup', (event) => {
-        if (event.keyCode === 13) createTask();
+        localStorage.setItem('Tasks', JSON.stringify(tasks));
     });
+}
 
-    btnAdd.addEventListener('click', createTask);
+function checkedTask(element) {
+    element.onchange = function() {
+        if (element.checked) element.parentElement.classList.add('checked'); 
+        else element.parentElement.classList.remove('checked'); 
+    };
+}
 
-    btnReset.addEventListener('click', () => {
-        ul.innerHTML = '';
-        localStorage.removeItem('Tasks');
-    });
+function editTask(element) {
+    element.addEventListener('click', openModalWindow);
+}
 
-    function openModalWindow(event) {
-        container.classList.remove('fadeIn');
+input.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) createTask();
+});
 
-        let modalWindow = document.createElement('div'),
-            modalWindowWrapper = document.createElement('div'),
-            modalWindowInput = document.createElement('input'),
-            buttonClose = document.createElement('i'),
-            buttonSave = document.createElement('button'),
-            overlay = document.createElement('div');
+btnAdd.addEventListener('click', createTask);
 
-        document.body.appendChild(modalWindow);
-        modalWindow.classList.add('modalWindow');
+btnReset.addEventListener('click', () => {
+    ul.innerHTML = '';
+    localStorage.removeItem('Tasks');
+});
 
-        document.body.appendChild(overlay);
-        overlay.classList.add('overlay');
+function openModalWindow(event) {
+    container.classList.remove('fadeIn');
 
-        modalWindow.appendChild(modalWindowWrapper);
-        modalWindowWrapper.classList.add('modalWindowWrapper');
-            
-        modalWindowWrapper.appendChild(modalWindowInput);
-        modalWindowInput.setAttribute('type', 'text');
-        modalWindowInput.setAttribute('id', 'textEdit');
-        modalWindowInput.value = event.target.parentElement.querySelector('p').textContent;
+    let modalWindow = document.createElement('div'),
+        modalWindowWrapper = document.createElement('div'),
+        modalWindowInput = document.createElement('input'),
+        buttonClose = document.createElement('i'),
+        buttonSave = document.createElement('button'),
+        overlay = document.createElement('div');
 
-        modalWindowWrapper.appendChild(buttonSave);
-        buttonSave.classList.add('save');
-        buttonSave.innerHTML = 'Save the changes';
+    document.body.appendChild(modalWindow);
+    modalWindow.classList.add('modalWindow');
 
-        function closeModalWindow() {
-            tasks.map((item, index, array) => {
-                if (event.target.parentElement.querySelector('p').textContent === item) {
-                    return array.splice(index, 1, modalWindowInput.value);
-                } 
-            });
-            localStorage.setItem('Tasks', JSON.stringify(tasks));
-            event.target.parentElement.querySelector('p').innerHTML = modalWindowInput.value;
-            overlay.remove();
-            modalWindow.remove();
-            container.classList.add('fadeIn');
-        }
+    document.body.appendChild(overlay);
+    overlay.classList.add('overlay');
 
-        buttonSave.addEventListener('click', closeModalWindow);
-
-        modalWindowInput.addEventListener('keyup', (event) => {
-            if (event.keyCode === 13) closeModalWindow();
-        });
-
-        modalWindowWrapper.appendChild(buttonClose);
-        buttonClose.classList.add('fa');
-        buttonClose.classList.add('fa-times');
+    modalWindow.appendChild(modalWindowWrapper);
+    modalWindowWrapper.classList.add('modalWindowWrapper');
         
-        buttonClose.addEventListener('click', function() {
-            overlay.remove();
-            modalWindow.remove();
-            container.classList.add('fadeIn');
-        });
-    }
-};
+    modalWindowWrapper.appendChild(modalWindowInput);
+    modalWindowInput.setAttribute('type', 'text');
+    modalWindowInput.setAttribute('id', 'textEdit');
+    modalWindowInput.value = event.target.parentElement.querySelector('p').textContent;
 
-window.addEventListener('load', ToDoList);
+    modalWindowWrapper.appendChild(buttonSave);
+    buttonSave.classList.add('save');
+    buttonSave.innerHTML = 'Save the changes';
+
+    function closeModalWindow() {
+        tasks.map((item, index, array) => {
+            if (event.target.parentElement.querySelector('p').textContent === item) {
+                return array.splice(index, 1, modalWindowInput.value);
+            } 
+        });
+        localStorage.setItem('Tasks', JSON.stringify(tasks));
+        event.target.parentElement.querySelector('p').innerHTML = modalWindowInput.value;
+        overlay.remove();
+        modalWindow.remove();
+        container.classList.add('fadeIn');
+    }
+
+    buttonSave.addEventListener('click', closeModalWindow);
+
+    modalWindowInput.addEventListener('keyup', (event) => {
+        if (event.keyCode === 13) closeModalWindow();
+    });
+
+    modalWindowWrapper.appendChild(buttonClose);
+    buttonClose.classList.add('fa');
+    buttonClose.classList.add('fa-times');
+    
+    buttonClose.addEventListener('click', function() {
+        overlay.remove();
+        modalWindow.remove();
+        container.classList.add('fadeIn');
+    });
+}
+
+const switchButton = document.createElement('div');
+
+switchButton.classList.add('switch-btn');
+
+switchButton.addEventListener('click', () => {
+    switchButton.classList.toggle('switch-on');
+    document.body.classList.toggle('dark-theme');
+});
+
+section.insertBefore(switchButton, container);
